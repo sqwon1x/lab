77c138b66195ae6d404ef2fcb92b4bab49b86d8c
sqwon1x <miichail.gutych@gmail.com>
2025-11-24 00:55:25 +0200

    sqwon1x
    lab3(all)

--- obj/Release/net9.0/lab1.AssemblyInfo.cs
+++ obj/Release/net9.0/lab1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c400b86223993fe1e2c5da7e1c9862743bd3abf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67109b7abec918ab52e73b216e8c82ad2918917b")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
